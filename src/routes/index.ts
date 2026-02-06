6d28e3e4f7ec42b7a4251b3603c6c284eb88ef20
Vitisadi <82157788+Vitisadi@users.noreply.github.com>
2026-02-06 17:47:28 -0500

    Added login, register user, register device endpoints

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,9 +3,11 @@ import type { dependency } from "../types/dependency.d.ts";
 import { registerRoot } from "./root.ts";
 import { registerHealth } from "./health.ts";
 import { registerDevice } from "./device.ts";
+import { registerAuth } from "./auth.ts";
 
 export function registerRoutes(app: Hono, deps: dependency) {
     registerRoot(app);
     registerHealth(app, deps);
     registerDevice(app, deps);
+    registerAuth(app, deps);
 }
